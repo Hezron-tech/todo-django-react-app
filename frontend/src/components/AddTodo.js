66077b8594bd4513a6f43b659e36d19c4965a231
Hezron-tech <kiprophez@gmail.com>
2022-05-28 16:42:45 +0300

    making request

--- frontend/src/components/AddTodo.js
+++ frontend/src/components/AddTodo.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import { Form,Button } from 'react-bootstrap'
-const AddTodo = () =>{
-    return( 
-        <Form>
-            <Form.Group controlId='title'>
-                <Form.Label>Title</Form.Label>
-                <Form.Control type ='text' placeholder='Enter Todo Title'/>
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
-            </Form.Group>
+const AddTodo = ({ addTodo }) => {
+	const [title, setTitle] = useState('')
+	const [description, setDescription] = useState('')
 
-            <Form.Group controlId='description'>
-                <Form.Label>Description</Form.Label>
-                <Form.Control type ='text' placeholder='Enter Description Title'/>
+	const addTodoHandler = e => {
+		e.preventDefault()
+		addTodo({
+			title,
+			description,
+			completed: false,
+		})
+	}
 
-            </Form.Group>
+	return (
+		<Form>
+			<Form.Group controlId='title'>
+			  <Form.Label>Title</Form.Label>
+			  <Form.Control type='text' placeholder='Enter Todo Title' onChange={e => setTitle(e.target.value)} />
+			</Form.Group>
 
-            <Button variant ='primary' type ='submit'>Add Todo</Button>
+			<Form.Group controlId='description'>
+			  <Form.Label>Description</Form.Label>
+			  <Form.Control type='text' placeholder='Enter Description' onChange={e => setDescription(e.target.value)} />
+			</Form.Group>
 
-
-        </Form>
-    )
+			<Button variant='primary' type='submit' onClick={addTodoHandler}>Add Todo</Button>
+		</Form>
+	)
 }
-export default AddTodo;
+
+export default AddTodo
